3f500650761ebddc15c4f7d3299ad0b528f8df90
sreekanth <sreekanthmandalaa@gmail.com>
2021-04-20 08:03:41 +0530

    wrap up

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,8 +1,9 @@
 import { CreateBookingComponent } from './../../../bookings/create-booking/create-booking.component';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ModalController, NavController } from '@ionic/angular';
 import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-place-detail',
@@ -12,20 +13,50 @@ import { Place } from '../../place.model';
 export class PlaceDetailPage implements OnInit {
   place: Place;
   constructor(
+    private route: ActivatedRoute,
     private router: Router,
     private navCtrl: NavController,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private placesService: PlacesService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+    });
+  }
 
-  onBookPlace() {
+  // onBookPlace() {
     // this.router.navigateByUrl('/places/tabs/discover');
     // this.navCtrl.navigateBack('/places/tabs/discover');
-    this.modalCtrl
-      .create({ component: CreateBookingComponent })
-      .then((modalEl) => {
-        modalEl.present();
-      });
-  }
+    // this.modalCtrl
+    //   .create({ component: CreateBookingComponent, componentProps : {selectedPlace: this.place,
+      
+    //   }
+    //   })
+    //   .then((modalEl) => {
+    //     modalEl.present();
+    //   });
+
+       onBookPlace() {
+        this.modalCtrl.create({
+          component: CreateBookingComponent,
+          componentProps : {selectedPlace: this.place},
+          cssClass: 'my-custom-class'
+        }).then(modelEl=>{
+          modelEl.present();
+          return modelEl.onDidDismiss();
+        })
+        .then(resultData=>{
+          console.log(resultData.data, resultData.role);
+          if(resultData.role === 'confirm')
+          console.log('booked!');
+        })
+        
+      }
+  
 }
